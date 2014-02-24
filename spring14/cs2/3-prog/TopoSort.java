75d34d3deda5e8aea55d54f0c7b59309ad2167b7
ben <bfeistmann@gmail.com>
2014-02-24 17:58:38 -0500

    prog 3 - dfs finished?

--- spring14/cs2/3-prog/TopoSort.java
+++ spring14/cs2/3-prog/TopoSort.java
@@ -15,6 +15,7 @@ public class TopoSort {
 	public static void main(String args[]) throws IOException   {
 
 		int i, j, k, numGraphs, vertex = 0, numNodes; /////////// Change the vertex ==> 'vertex' after all function are written //
+		Deque<Integer> stack = new ArrayDeque<Integer>();
 
 		// create a Graph object
 		TopoSort graph = new TopoSort();
@@ -36,7 +37,7 @@ public class TopoSort {
 					adjMatrix[j][k] = in.nextInt();
 				}
 			}
-	
+			
 			/*
 			// Perform Topological Sort	
 			if (graph.isSortable(adjMatrix)) {
@@ -51,12 +52,12 @@ public class TopoSort {
 
 	}
 
-	public void DFS(int vertex, int numNodes, int[][] adjMatrix) {
+	public int[] DFS(int vertex, int numNodes, int[][] adjMatrix) {
 
 		int[] visited = new int[numNodes];
 		int[] result = new int[numNodes];
 		Deque<Integer> stack = new ArrayDeque<Integer>();
-		int i, j;
+		int x, i, j, k = 0;
 
 		// Initialize every node to unvisited for start of search.
 		for (i = 0; i < numNodes; i++)	
@@ -67,21 +68,53 @@ public class TopoSort {
 
 		// Mark as visited.
 		visited[vertex] = 1;	
-	
+
 		// Main Loop for traversing the directed graph.
-		for (i = 0; i < numNodes; i++) {
-			for (j = 0; j < numNodes; j++) {
-				if (adjMatrix[i][j] == 1 && visited[adjMatrix[i][j]] == 0) {
+		for(i = 0; i < numNodes; i++) {
+			for(j = 0; j < numNodes; j++) {
+
+				// Check there is a connection from current vertex to next node
+				// and that the node has not been visited.
+				if (adjMatrix[i][j] == 1 && visited[ adjMatrix[i][j] ] == 0) {
 					vertex = adjMatrix[i][j];		// update the vertex
 					stack.push(vertex);				// push the vertex on the stack
 					visited[vertex] = 1;			// mark the vertex as visited
 					i = vertex;						// update the outer loop to the new vertex
 					j = -1;							// reset the inner loop to 'zero' --> really -1
 				}									// because the for-loop will interate the j-value to zero.
+				
+				// Current vertex has no children (dead end).
+				if (j == numNodes-1) {
+					result[k++] = stack.peek();		// add node to be popped to the result array.
+					stack.pop();					// pop the current node (dead end).
 					
-			}
-		}
+					// Check if the stack is not empty	
+					if (stack.peek() != null) {
+						vertex = stack.peek();			// update the vertex with the node on top of the stack.	
+					}
 
-	}
+					// The stack is empty.
+					else {
+						// Find the next unvisited node to be the new vertex.
+						for (x = 0; x < numNodes; x++) {
+							if (visited[i] = 0) {
+								vertex = visited[i];		// update the new vertex.
+								stack.push(vertex);			// push new vertex on the stack;
+								j = -1;						// reset the inner loop to 'zero' --> really -1
+							}								// because the for-loop will interate the j-value to zero.
+						}
+					}
+				}
+			} // end inner-loop
+		} // end out-loop 
+
+		return result;
+	} // end DFS()
 
 } // end class 
+
+
+
+
+
+
