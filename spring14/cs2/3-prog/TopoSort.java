26254a5c0b289a9730df33b612bfb32bd51e0ade
ben <bfeistmann@gmail.com>
2014-02-25 20:32:01 -0500

    Prog 3 - both test cases match

--- spring14/cs2/3-prog/TopoSort.java
+++ spring14/cs2/3-prog/TopoSort.java
@@ -14,10 +14,10 @@ public class TopoSort {
 
 	public static void main(String args[]) throws IOException   {
 
-		int i, j, k, numGraphs, vertex = 0, numNodes; /////////// Change the vertex ==> 'vertex' after all function are written //
+		int i, j, k, numGraphs, vertex = 0, numNodes;
 
 		// Read in the file.
-		Scanner in = new Scanner( new File("test.txt") );
+		Scanner in = new Scanner( new File("graphs.txt") );
 
 		numGraphs = in.nextInt();
 
@@ -35,14 +35,14 @@ public class TopoSort {
 			if (DFS(vertex, numNodes, adjMatrix) != null) {
 				System.out.print("TS(" + i + ",DFS): ");
 				printResult(TS_DFS(DFS(vertex, numNodes, adjMatrix)));
-			//	System.out.print("TS(" + i + ",DBO): ");
-			//	printResult(TS_DBO(DBO(vertex, numNodes, adjMatrix)));
+				System.out.print("TS(" + i + ",DBO): ");
+				printResult(DBO(vertex, numNodes, adjMatrix));
 			}
 			else {
 				System.out.print("TS(" + i + ",DFS): ");
 				System.out.println("NO TOPOLOGICAL SORT");
-			//	System.out.print("TS(" + i + ",DBO): ");
-			//	printResult(TS_DBO(DBO(vertex, numNodes, adjMatrix)));
+				System.out.print("TS(" + i + ",DBO): ");
+				System.out.println("NO TOPOLOGICAL SORT");
 			}
 		}
 	}
@@ -136,29 +136,46 @@ public class TopoSort {
 	public static int[] DBO(int vertex, int numNodes, int[][] adjMatrix) { 
 		Queue <Integer> nodes = new LinkedList<Integer>();	
 		int[] result = new int[numNodes];
-		int i, j;
-		
-		// Nested for loop for traversing adjacency matrix
-		for (i = 0; i < numNodes; i++) {
-			for (j = 0; j < numNodes; j++) {
-				
-				// Checks if node has an incoming edge
-				// Moves to next vertex if so
-				if (adjMatrix[j][i] == 1) {
-					break;
-				}
-				
-				// Checks if node does not have incoming edges
-				// Adds to the queue if so
-				else if (adjMatrix[j][i] == 0 && j == numNodes-1) {
-					nodes.offer(i);				// Enqueue vertex to the queue	
-				}
-			}
-		}
-	}
+		int[] visited = new int[numNodes];
+		int i, j, k = 0, counter = 0;
 
-	public static void printResult(int[] arr)
-	{
+		do  {	
+			// Nested for loop for traversing adjacency matrix
+			for (i = 0; i < numNodes; i++) {
+				for (j = 0; j < numNodes; j++) {
+					
+					// Checks if node has an incoming edge
+					// Moves to next vertex if so
+					if (adjMatrix[j][i] == 1 || visited[i] == 1) {
+						break;
+					}
+					
+					// Checks if node does not have incoming edges
+					// Adds to the queue if so
+					else if (adjMatrix[j][i] == 0 && j == numNodes-1) {
+						nodes.offer(i);				// Enqueue vertex to the queue
+						visited[i] = 1;				// Mark vertex as visited
+					}
+				} // end inner-loop
+			} // end outer-loop
+			
+			while (nodes.peek() != null) {
+				vertex = nodes.poll();				// Dequeue vertex from the queue
+				result[k++] = vertex;				// Add vertex to topological sort array
+				counter++;							// Update the counter
+
+				// Delete the outgoing edges of the vertex.
+				for (i = 0; i < numNodes; i++) {
+					adjMatrix[vertex][i] = 0;	
+				} 
+			} 
+		} while (counter != numNodes);
+
+		return result;
+
+	} // end DBO()
+
+	public static void printResult(int[] arr) {
 		int len = arr.length;
 
 		for (int i = 0; i < len; i++)
@@ -167,9 +184,3 @@ public class TopoSort {
 	}	
 
 } // end class 
-
-
-
-
-
-
