0443481953d5c4f2546de0876bf1e6e16ac4e3d6
ben <bfeistmann@gmail.com>
2014-03-10 19:23:24 -0400

    properly reading in bit string

--- spring14/cs2/4-prog/Multiplication.java
+++ spring14/cs2/4-prog/Multiplication.java
@@ -14,7 +14,8 @@ public class Multiplication {
 	
 	public static void main(String args[]) throws IOException {
 		
-		int i, j, k, len, binNum;
+		int i, j, k, len;
+		String binNum;
 
 		// Read in the file.
 		Scanner in = new Scanner( new File("mult.txt") );
@@ -25,19 +26,24 @@ public class Multiplication {
 		// Loop through the file.	
 		for (i = 0; i < k; i++) {
 
-			// Read in length of binary number and initialize length of array.
+			System.out.println("---- First Binary Number ----");
+			// Read in length of the first binary number and initialize length of array.
 			len = in.nextInt();
 			int[] bin1 = new int[len];
 
 			// Read in the binary number.
-			binNum = in.nextInt(); 
-		
-			// Convert binary int to array.	
-			for (j = len-1; j >= 0; j--) {
-				bin1[j] = binNum%10;
-				binNum /= 10;	
-			}
+			binNum = in.next(); 
+			System.out.println(binNum);
+			
+			System.out.println("---- Second Binary Number ----");
+			// Read in length of the second binary number and initialize length of array.
+			len = in.nextInt();
+			int[] bin2 = new int[len];
 
+			// Read in the binary number.
+			binNum = in.next(); 
+			System.out.println(binNum);
+		
 		}
 	} // end main
 
