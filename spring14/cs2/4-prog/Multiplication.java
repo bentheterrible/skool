d402dbab76c4d6237a76e4f5002bd7466a136523
ben <bfeistmann@gmail.com>
2014-03-11 13:32:02 -0400

    successfully converted binary number to array

--- spring14/cs2/4-prog/Multiplication.java
+++ spring14/cs2/4-prog/Multiplication.java
@@ -14,7 +14,8 @@ public class Multiplication {
 	
 	public static void main(String args[]) throws IOException {
 		
-		int i, j, k, len;
+		int i, j, k, len, b;
+		char a;
 		String binNum;
 
 		// Read in the file.
@@ -26,30 +27,34 @@ public class Multiplication {
 		// Loop through the file.	
 		for (i = 0; i < k; i++) {
 
-			System.out.println("---- First Binary Number ----");
 			// Read in length of the first binary number and initialize length of array.
 			len = in.nextInt();
 			int[] bin1 = new int[len];
 
-			// Read in the binary number.
+			// Read in the binary number as a String.
 			binNum = in.next(); 
-			System.out.println(binNum);
-			char a = binNum.charAt(3);
-			int b = a-48;
-			System.out.println(b);
 			
-			System.out.println("---- Second Binary Number ----");
+			// Convert the binary number from a String to an int array.
+			for (j = 0; j < len; j++) {
+				a = binNum.charAt(j);		// grabs a specific character in the string
+				b = a-48;					// converts that character into an integer
+				bin1[j] = b;					// place each converters char->int in respective index
+			}
+			
 			// Read in length of the second binary number and initialize length of array.
 			len = in.nextInt();
 			int[] bin2 = new int[len];
 
 			// Read in the binary number.
 			binNum = in.next(); 
-			System.out.println(binNum);
-		
-			a = binNum.charAt(3);	
-			b = a-48;
-			System.out.println(b);
+			
+			// Convert the binary number from a String to an int array.
+			for (j = 0; j < len; j++) {
+				a = binNum.charAt(j);		// grabs a specific character in the string
+				b = a-48;					// converts that character into an integer
+				bin2[j] = b;					// place each converters char->int in respective index
+			}
+
 		}
 	} // end main
 
