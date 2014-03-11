235ba1f83837005612b4fad6811094839a4302a6
ben <bfeistmann@gmail.com>
2014-03-11 16:13:14 -0400

    working 2D matrix for adding what was multiplies (prog 4)

--- spring14/cs2/4-prog/Multiplication.java
+++ spring14/cs2/4-prog/Multiplication.java
@@ -21,52 +21,95 @@ public class Multiplication {
 		// Read in the file.
 		Scanner in = new Scanner( new File("mult.txt") );
 		
-		// Number of multiplications.
-		k = in.nextInt();
-		
-		// Loop through the file.	
-		for (i = 0; i < k; i++) {
-
-			// Read in length of the first binary number and initialize length of array.
-			len1 = in.nextInt();
-			int[] bin1 = new int[len1];
-
-			// Read in the binary number as a String.
-			binNum = in.next(); 
+			// Number of multiplications.
+			k = in.nextInt();
 			
-			// Convert the binary number from a String to an int array.
-			for (j = 0; j < len1; j++) {
-				a = binNum.charAt(j);		// grabs a specific character in the string
-				b = a-48;					// converts that character into an integer
-				bin1[j] = b;					// place each converters char->int in respective index
-			}
-			
-			// Read in length of the second binary number and initialize length of array.
-			len2 = in.nextInt();
-			int[] bin2 = new int[len2];
+			// Loop through the file.	
+			for (i = 0; i < k; i++) {
 
-			// Read in the binary number.
-			binNum = in.next(); 
-			
-			// Convert the binary number from a String to an int array.
-			for (j = 0; j < len2; j++) {
-				a = binNum.charAt(j);		// grabs a specific character in the string
-				b = a-48;					// converts that character into an integer
-				bin2[j] = b;					// place each converters char->int in respective index
-			}
+				// Read in length of the first binary number and initialize length of array.
+				len1 = in.nextInt();
+				int[] bin1 = new int[len1];
 
-			//////////////// 
-			// Algorithms //
-			//////////////// 
-			
-			// Standard Multiplication Algorithm
-			printArray(bin1);	
-			printArray(bin2);	
+				// Read in the binary number as a String.
+				binNum = in.next(); 
+				
+				// Convert the binary number from a String to an int array.
+				for (j = 0; j < len1; j++) {
+					a = binNum.charAt(j);		// grabs a specific character in the string
+					b = a-48;					// converts that character into an integer
+					bin1[j] = b;					// place each converters char->int in respective index
+				}
+				
+				// Read in length of the second binary number and initialize length of array.
+				len2 = in.nextInt();
+				int[] bin2 = new int[len2];
 
-		} // end main for-loop
-	} // end main
+				// Read in the binary number.
+				binNum = in.next(); 
+				
+				// Convert the binary number from a String to an int array.
+				for (j = 0; j < len2; j++) {
+					a = binNum.charAt(j);		// grabs a specific character in the string
+					b = a-48;					// converts that character into an integer
+					bin2[j] = b;				// place each converters char->int in respective index
+				}
+
+				//////////////// 
+				// Algorithms //
+				//////////////// 
+				
+				// Standard Multiplication Algorithm.
+				multiply(bin1, bin2, len1, len2);
+				
+				// Karatsuba's divide and conquer algorithm.
+				//printArray( karatsuba(bin1, bin2, len1, len2) );
+				
+			} // end main for-loop
+		} // end main
 
 	public static void multiply(int[] bin1, int[] bin2, int len1, int len2) {
+		int i, j = 0, temp,
+			n = 2 * (len2) - 1;
+		int[][] num_to_add = new int[len2][n];
+		int[] binary1 = new int[len1];
+		int[] binary2 = new int[len2];
+
+		binary1 = reverseArray(bin1);
+		binary2 = reverseArray(bin2);
+
+		for (i = 0; i < len2; i++) {
+			for (j = 0; j < len1; j++) {
+				num_to_add[i][i+j] = binary1[j] * binary2[i];
+			}
+		}
+
+		for (i = 0; i < len2; i++) {
+			for (j = 0; j < n; j++) {
+				System.out.print(num_to_add[i][j]);
+			}
+			System.out.println();
+		}
+
+
+		System.out.println();
+
+	}
+
+	public static int[] reverseArray(int[] array) {
+		int i, temp;
+		
+		for (i = 0; i < array.length / 2; i++) {
+			temp = array[i];
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = temp;	
+		}
+
+		return array;
+	}
+
+	
+	public static void karatsuba(int[] bin1, int[] bin2, int len1, int len2) {
 	}
 
 	public static void printArray(int[] arr) {
