7caebd633a055cae4975bbb5d55d5d1b468a6b7b
ben <bfeistmann@gmail.com>
2014-04-07 18:38:34 -0400

    load function loads the dummy variable

--- spring14/cs2/5-prog/Heap.java
+++ spring14/cs2/5-prog/Heap.java
@@ -38,7 +38,7 @@ public class Heap {
 			switch(in.next()) {
 				case "load":
 					ArrayList<Integer> list = new ArrayList<Integer>(heap.load(in));
-					int[] array = new int[list.size()];
+					heap.array = new int[list.size()+1];
 
 					heap.array = heap.convert_list(list); // converts array list to array
 					break;
@@ -46,7 +46,7 @@ public class Heap {
 					heap.print_array(heap.array);
 					break;
 				case "build-heap":
-					heap.build_heap(heap.array);
+					heap.build_heap();
 					break;
 				case "delete-max":
 					heap.delete_max();
@@ -79,10 +79,26 @@ public class Heap {
 		print_array(array);
 	}
 
-	public int[] build_heap(int[] array) {
-		int len = array.length;	
-		
-		return array;
+	public int[] build_heap() {
+		int n = this.array.length-1;	
+		for (int i = n/2; i >= 0; i--) {
+			int k = i;
+			int v = this.array[k];
+			boolean heap = false;
+			while (!heap && 2*k <= n) {
+				int j = 2*k;
+				if (j < n && this.array[j] < this.array[j+1])
+					j++;
+				if (v >= this.array[j])
+					heap = true;
+				else {
+					this.array[k] = this.array[j];
+					k = j;
+				}
+				this.array[k] = v;
+			}
+		}
+		return this.array;
 	}
 	
 	public void delete_max() {
@@ -99,17 +115,29 @@ public class Heap {
 	//////////////////////////////////		
 	// 		Helper Methods          //
 	//////////////////////////////////
+	
+	public static int parent_index(int[] array, int i) {
+		return (i-1)/2;
+	}
 		
-	public static int get_parent(int[] array, int i) {
+	public static int parent_val(int[] array, int i) {
 		return array[(i-1)/2];
 	}
 
-	public static int get_lc(int[] array, int i) {
+	public static int lc_val(int[] array, int i) {
 		return array[2*i+1];
 	}
 
-	public static int get_rc(int[] array, int i) {
+	public static int lc_index(int[] array, int i) {
+		return 2*i+1;
+	}
+
+	public static int rc_val(int[] array, int i) {
 		return array[2*i+2];
+	}
+
+	public static int rc_index(int[] array, int i) {
+		return 2*i+2;
 	}
 
 	public static int[] swap(int[] array, int index1, int index2) {
@@ -134,12 +162,19 @@ public class Heap {
 	// Converts an ArrayList to an primative int array (int[]).
 	public int[] convert_list(ArrayList<Integer> list) {
 		int[] ret = new int[list.size()];
+		int i;
 
-		for (int i = 0; i < ret.length; i++) {
+		for (i = 0; i < ret.length; i++) {
 			ret[i] = list.get(i).intValue();
 		}
 
-	return ret;
-	}
+		int[] fin = new int[ret.length+1];
+		fin[0] = -1;
+		for (i = 1; i < fin.length; i++) {
+			fin[i] = ret[i-1];
+		}
 
+	return fin;
+	}
+	
 } // end class
--- spring14/cs2/5-prog/Test.java
+++ spring14/cs2/5-prog/Test.java
@@ -3,15 +3,13 @@ import java.io.*;
 import java.util.*;
 
 public class Test{
+		
+	private int[] array;
 
 	public static void main (String args[]) throws IOException  {
 
 		Test test = new Test();
-		
+
 	}
 
-	public int add(int n) {
-
-		return n+1;
-	}
 }
